62e0abb2f6e00a386672bde9d75bde4f72458bb9
smokemoney81 <kaisaschnitt99@gmail.com>
2026-03-28 11:14:48 +0100

    fix: redeploy with env vars

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,2 +1,3 @@
+// Supabase client
 import { createClient } from '@supabase/supabase-js';
 export const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_ANON_KEY);
